681916186f75b3cc25a977f8b815b46ac0cfe02e
maxus <maxus.info.mail@gmail.com>
2023-05-22 10:57:15 +0300

    Better logging

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,14 @@ pub mod watching;
 
 use std::path::PathBuf;
 
+use chrono::Local;
 use clap::Parser;
-use tracing_subscriber::{prelude::__tracing_subscriber_SubscriberExt, util::SubscriberInitExt};
+
+use tracing_subscriber::{
+    fmt::{time::FormatTime, SubscriberBuilder},
+    prelude::__tracing_subscriber_SubscriberExt,
+    util::SubscriberInitExt,
+};
 
 /// A tool to watch and compile minecraft resourcepacks
 #[derive(Parser)]
@@ -21,6 +27,7 @@ enum Args {
 #[derive(Parser)]
 pub struct WatchCmd {
     /// Root directory to watch
+    #[clap(default_value = ".")]
     root_dir: PathBuf,
     /// Specifies the output file
     #[arg(long = "out", short, default_value = "pack.zip")]
@@ -33,6 +40,7 @@ pub struct WatchCmd {
 #[derive(Parser)]
 pub struct CompileCmd {
     /// Root directory to build
+    #[clap(default_value = ".")]
     root_dir: PathBuf,
     /// Specifies the output file
     #[arg(long = "out", short, default_value = "pack.zip")]
@@ -42,19 +50,37 @@ pub struct CompileCmd {
     // packsquash: bool,
 }
 
+#[derive(Debug, Clone)]
+struct TimeFmt;
+
+impl FormatTime for TimeFmt {
+    fn format_time(&self, w: &mut tracing_subscriber::fmt::format::Writer<'_>) -> std::fmt::Result {
+        w.write_fmt(format_args!("{}", Local::now().format("%H:%M:%S")))
+    }
+}
+
 #[tracing::instrument]
 fn main() -> anyhow::Result<()> {
     // Logging
-    tracing_subscriber::registry()
-        .with(tracing_subscriber::fmt::layer().compact().with_ansi(true))
-        .init();
+    let filter = tracing_subscriber::filter::filter_fn(|meta| {
+        let is_rpd = meta.module_path().unwrap_or_default().contains("rpd")
+            && *meta.level() <= tracing::Level::INFO;
+        is_rpd || *meta.level() <= tracing::Level::WARN
+    });
+    let subscriber = SubscriberBuilder::default()
+        .compact()
+        .with_ansi(true)
+        .with_timer(TimeFmt)
+        .finish();
+    subscriber.with(filter).init();
 
     // Args
     let args = Args::parse();
-
     match args {
         Args::Watch(watch_cmd) => {
-            watching::watch(watch_cmd)?;
+            if let Err(error) = watching::watch(watch_cmd) {
+                tracing::error!("Failed to watch directory: {error}")
+            }
         }
         Args::Compile(compile_cmd) => {
             if let Err(error) = comp::compile_pack(compile_cmd.root_dir, compile_cmd.out_file) {
